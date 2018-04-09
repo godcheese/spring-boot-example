4808a8a69165beda41ee129aa9b613ca657a258f
godcheese <godcheese@outlook.com>
2018-04-09 09:27:35 +0800

    Initial commit

--- git_push.bat
+++ git_push.bat
@@ -3,11 +3,11 @@ CHCP 65001
 echo.author godcheese [godcheese@outlook.com]
 set "CURRENT_DIR=%~dp0"
 cd %CURRENT_DIR%
-cd ..
+
 echo.正在添加文件...
 call git add .
 set remark=Initial commit
-set /p remark=正在提交备注...，请填写备注（可空）:
+set /p remark=正在提交备注...，请填写备注（Initial commit）:
 call git commit -m "%remark%"
 echo.正在开始提交代码...
 call git push origin master
