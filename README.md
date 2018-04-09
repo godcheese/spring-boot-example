56a337115e6b696db4cea4d373ff52d2d6a32a19
godcheese <godcheese@outlook.com>
2018-04-09 09:10:43 +0800

    常规提交

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # springboot-example
-> 自己写的 Spring Boot 学习例子
-
+> Spring Boot 学习例子
 
 ## Spring Boot 1.x
-
-### springsecurityforspringboot1
+### [springboot1springsecurity](/tree/master/springboot1springsecurity)
 > Spring Boot 1.x 整合 Spring Security 例子
-
--- scripts/git_push.bat
+++ scripts/git_push.bat
@@ -6,8 +6,8 @@ cd %CURRENT_DIR%
 cd ..
 echo.正在添加文件...
 call git add .
-set remark="Initial commit"
-set /p remark=正在提交备注...，请填写备注（Initial commit）:
+set remarks=常规提交
+set /p remarks=正在提交备注...，请填写备注（可空）:
 call git commit -m "%remarks%"
 echo.正在开始提交代码...
 call git push origin master
