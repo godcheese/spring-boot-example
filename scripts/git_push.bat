735692986e3a47df814a5f42ac400975ba1ee85f
godcheese <godcheese@outlook.com>
2018-04-09 09:15:05 +0800

    Initial~commit

--- scripts/git_push.bat
+++ scripts/git_push.bat
@@ -6,9 +6,9 @@ cd %CURRENT_DIR%
 cd ..
 echo.正在添加文件...
 call git add .
-set remarks=常规提交
-set /p remarks=正在提交备注...，请填写备注（可空）:
-call git commit -m "%remarks%"
+set remark="Initial~commit"
+set /p remark=正在提交备注...，请填写备注（可空）:
+call git commit -m "%remark%"
 echo.正在开始提交代码...
 call git push origin master
 echo.代码提交成功，正在关闭...
